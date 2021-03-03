adddfd797af0b952b81a41a4d809422c66c7ab87
Juan Brugera Monedero <ed3n35@gmail.com>
2021-03-03 16:05:27 +0100

    feat: ssh config

--- scripts/quickstart.py
+++ scripts/quickstart.py
@@ -1,25 +1,14 @@
 #!/usr/bin/python
 
 import os
+import shutil
 import subprocess
+import sys
 import time
 
 from datetime import datetime, timedelta
 from typing import Callable
 from pathlib import Path
-
-HOME = Path.home()
-SSH_PATH = os.path.join(HOME, '.ssh')
-USER = 'terra'
-PASSWORD = '@._admin4dm1n#'
-VIRTUAL_MACHINES = [
-    "nfs-jbrug91.westeurope.cloudapp.azure.com",
-    "k8smaster-jbrug91.westeurope.cloudapp.azure.com",
-    "k8sworker0-jbrug91.westeurope.cloudapp.azure.com",
-    "k8sworker1-jbrug91.westeurope.cloudapp.azure.com"
-]
-
-is_sh: Callable[[str], bool] = lambda file: file.endswith('.sh')
 
 
 def parent_path(path: str, level: int = 1) -> str:
@@ -30,6 +19,24 @@ def parent_path(path: str, level: int = 1) -> str:
         return parent_path(parent, level - 1)
 
 
+HOME = Path.home()
+SSH_PATH = os.path.join(HOME, '.ssh')
+BOOTSTRAP_PATH = os.path.abspath(__file__)
+PROJECT_PATH = parent_path(BOOTSTRAP_PATH, 2)
+
+SCRIPTS_PATH = os.path.join(PROJECT_PATH, 'scripts')
+USER = 'terra'
+PASSWORD = r'@._admin4dm1n#'
+VIRTUAL_MACHINES = [
+    "nfs-jbrug91.westeurope.cloudapp.azure.com",
+    "k8smaster-jbrug91.westeurope.cloudapp.azure.com",
+    "k8sworker0-jbrug91.westeurope.cloudapp.azure.com",
+    "k8sworker1-jbrug91.westeurope.cloudapp.azure.com"
+]
+
+is_sh: Callable[[str], bool] = lambda file: file.endswith('.sh')
+
+
 def ssh_copy_id(vm: str):
     id_rsa_pub = os.path.join(SSH_PATH, 'id_rsa.pub')
     args = ["ssh-copy-id", "-i", id_rsa_pub, f"{USER}@{vm}"]
@@ -38,33 +45,37 @@ def ssh_copy_id(vm: str):
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
 
-    answers = ['yes', PASSWORD]
-
-    for answer in answers:
-        time.sleep(1)
-        proc.stdin.write("{}\n".format(answer).encode())
-        proc.stdin.flush()
+    time.sleep(0.5)
+    proc.stdin.write("{}\n".format(PASSWORD).encode())
+    time.sleep(0.5)
+    proc.stdin.flush()
 
 
 if __name__ == '__main__':
+    begin_at = int(sys.argv[1])
     options = {'0': 'apply and deploy', '1': 'destroy'}
     for k, v in options.items():
         print("{}: {}".format(k, v))
     option = input("Please select an option: ")
 
-    bootstrap_path = os.path.abspath(__file__)
-    project_path = parent_path(bootstrap_path, 2)
+    sh_files = list(sorted(filter(is_sh, os.listdir(SCRIPTS_PATH))))
 
-    scripts_path = os.path.join(project_path, 'scripts')
-    sh_files = list(sorted(filter(is_sh, os.listdir(scripts_path))))
-
-    for sh_file in sh_files:
+    for sh_file in sh_files[begin_at:]:
         if sh_file.startswith(option):
-            sh_file_path = os.path.join(scripts_path, sh_file)
+            sh_file_path = os.path.join(SCRIPTS_PATH, sh_file)
             # add rwxr--r-- permissions to sh files
             os.chmod(sh_file_path, 0o744)
             if "ansible_master" in sh_file:
                 output = subprocess.check_output([sh_file_path]).decode()
+            elif "ansible_nfs" in sh_file:
+                known_hosts = os.path.join(SSH_PATH, 'known_hosts')
+                shutil.copy(os.path.join(PROJECT_PATH, 'files', '.ssh', 'config'),
+                            os.path.join(SSH_PATH, 'config'))
+
+                if os.path.exists(known_hosts):
+                    os.remove(known_hosts)
+                for virtual_machine in VIRTUAL_MACHINES:
+                    ssh_copy_id(virtual_machine)
             else:
                 status = subprocess.check_call([sh_file_path])
                 if ("terraform_apply" in sh_file) and (status == 0):
@@ -72,6 +83,3 @@ if __name__ == '__main__':
                     print(f"Waiting {minutes} minutes for DNS refreshing...")
                     print(f"Execution will continue @ {datetime.now() + timedelta(minutes=minutes)}")
                     time.sleep(minutes * 60)
-                    os.remove(os.path.join(SSH_PATH, 'known_hosts'))
-                    for virtual_machine in VIRTUAL_MACHINES:
-                        ssh_copy_id(virtual_machine)
