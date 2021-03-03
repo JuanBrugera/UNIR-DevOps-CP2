d45b5f59c97dbd496644cd4d1408eec9317a2e36
Juan Brugera Monedero <ed3n35@gmail.com>
2021-03-03 17:42:28 +0100

    feat: workers template

--- scripts/quickstart.py
+++ scripts/quickstart.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import re
 import shutil
 import subprocess
 import sys
@@ -24,6 +25,12 @@ SSH_PATH = os.path.join(HOME, '.ssh')
 BOOTSTRAP_PATH = os.path.abspath(__file__)
 PROJECT_PATH = parent_path(BOOTSTRAP_PATH, 2)
 SCRIPTS_PATH = os.path.join(PROJECT_PATH, 'scripts')
+ANSIBLE_PATH = os.path.join(PROJECT_PATH, 'ansible')
+
+TOKEN_REGEX = re.compile(r'--token (\w{6}\.\w{16})')
+DISCOVERY = re.compile(r'--discovery-token-ca-cert-hash (sha256:\w{64})')
+
+MINUTES = 3
 
 is_sh: Callable[[str], bool] = lambda file: file.endswith('.sh')
 
@@ -43,13 +50,25 @@ if __name__ == '__main__':
     for sh_file in sh_files[begin_at:]:
         if sh_file.startswith(option):
             sh_file_path = os.path.join(SCRIPTS_PATH, sh_file)
-            # add rwxr--r-- permissions to sh files
             os.chmod(sh_file_path, 0o744)
             if "ansible_master" in sh_file:
-                # output = subprocess.check_output([sh_file_path]).decode()
-                subprocess.check_call([sh_file_path])
-            elif "ansible_nfs" in sh_file:
-                known_hosts = os.path.join(SSH_PATH, 'known_hosts')
+                output = subprocess.check_output([sh_file_path]).decode()
+                token = TOKEN_REGEX.search(output)
+                discovery = DISCOVERY.search(output)
+                if (token is None) or (discovery is None):
+                    print(token, discovery, output)
+                    raise ValueError("Token or Discovery not found")
+                else:
+                    token_value = token.group(1)
+                    discovery_value = discovery.group(1)
+                    with open(os.path.join(ANSIBLE_PATH, 'workers.yml.template')) as source:
+                        data = source.read()
+                        with open(os.path.join(ANSIBLE_PATH, 'workers.yml')) as destination:
+                            destination.write(
+                                data.replace("@DISCOVERY@", discovery_value).replace("@TOKEN@", token_value))
+                            destination.close()
+                        source.close()
+            elif "ansible" in sh_file:
                 shutil.copy(
                     os.path.join(PROJECT_PATH, 'files', '.ssh', 'config'),
                     os.path.join(SSH_PATH, 'config')
@@ -57,7 +76,6 @@ if __name__ == '__main__':
             else:
                 status = subprocess.check_call([sh_file_path])
                 if ("terraform_apply" in sh_file) and (status == 0):
-                    minutes = 5
-                    print(f"Waiting {minutes} minutes for DNS refreshing...")
-                    print(f"Execution will continue @ {datetime.now() + timedelta(minutes=minutes)}")
-                    time.sleep(minutes * 60)
+                    print(f"Waiting {MINUTES} minutes for VMs 100% working...")
+                    print(f"Execution will continue @ {datetime.now() + timedelta(minutes=MINUTES)}")
+                    time.sleep(MINUTES * 60)
